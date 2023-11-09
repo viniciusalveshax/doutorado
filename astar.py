d669be7f0690b2dca84a6c53b6f5e07d2d8e65ed
Vinícius Alves Hax <viniciushax@gmail.com>
2023-11-09 17:48:47 -0300

    Passando a utilização da classe para um exemplo separado. Deixando o arquivo astar.py somente para a implementação da classe.

--- astar.py
+++ astar.py
@@ -186,21 +186,3 @@ class AStar:
 			return False
 	
 
-map_file = open('map.txt', 'r')
-    
-# Inside rooms    
-start = (3, 6)
-end = (7, 35)
-
-problem = AStar(map=map_file, start=start, end=end)
-problem.print_map()
-
-# Return true if there is a path
-if problem.solve() == True:
-	print("Found a way")
-	problem.print_map_with_solution()
-	path_from_1_to_2 = problem.get_path()
-else:
-	print("There is no path between start and end")
-	
-
---
+++ astar_usage.py
@@ -0,0 +1,20 @@
+from astar import AStar
+
+map_file = open('map.txt', 'r')
+    
+# Inside rooms    
+start = (3, 6)
+end = (7, 35)
+
+problem = AStar(map=map_file, start=start, end=end)
+#problem.print_map()
+
+# Return true if there is a path
+if problem.solve() == True:
+	print("Found a way")
+	problem.print_map_with_solution()
+	path_from_1_to_2 = problem.get_path()
+else:
+	print("There is no path between start and end")
+	
+
