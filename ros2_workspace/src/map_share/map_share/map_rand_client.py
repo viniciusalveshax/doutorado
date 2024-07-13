7ff39a171abe26aa94a5d34bb6a1774ff87419f9
Vinícius Alves Hax <viniciushax@gmail.com>
2024-07-13 17:00:50 -0300

    Melhorando nomes de variáveis do exemplo

--- ros2_workspace/src/map_share/map_share/map_rand_client.py
+++ ros2_workspace/src/map_share/map_share/map_rand_client.py
@@ -9,12 +9,12 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        self.cli = self.create_client(GetRand, 'add_two_ints')
+        self.cli = self.create_client(GetRand, 'get_rand_int')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
         self.req = GetRand.Request()
 
-    def send_request(self, a, b):
+    def send_request(self):
         #self.req.a = a
         #self.req.b = b
         return self.cli.call_async(self.req)
@@ -24,12 +24,12 @@ def main():
     rclpy.init()
 
     minimal_client = MinimalClientAsync()
-    future = minimal_client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+    future = minimal_client.send_request()
     rclpy.spin_until_future_complete(minimal_client, future)
     response = future.result()
     minimal_client.get_logger().info(
-        'Result of add_two_ints: for %d + %d = %d' %
-        (int(sys.argv[1]), int(sys.argv[2]), response.sum))
+        'Número aleatório %d' %
+        (response.number))
 
     minimal_client.destroy_node()
     rclpy.shutdown()
--- ros2_workspace/src/map_share/map_share/map_rand_server.py
+++ ros2_workspace/src/map_share/map_share/map_rand_server.py
@@ -9,14 +9,16 @@ class MinimalService(Node):
 
     def __init__(self):
         super().__init__('minimal_service')
-        self.srv = self.create_service(GetRand, 'add_two_ints', self.add_two_ints_callback)
+        self.srv = self.create_service(GetRand, 'get_rand_int', self.get_rand_int_callback)
 
-    def add_two_ints_callback(self, request, response):
+    def get_rand_int_callback(self, request, response):
         #response.sum = request.a + request.b
         #self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
 
 	# Retorna como resposta um número aleatório entre 0 e 99
-        response.sum = random.randint(0, 99)
+        response.number = random.randint(0, 99)
+
+        self.get_logger().info('Respondendo pedido com valor %d' % (response.number))
 
         return response
 
