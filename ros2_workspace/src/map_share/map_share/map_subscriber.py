4acd4ead3e7394d8e35dbd380817300fc886dde3
Vinícius Alves Hax <viniciushax@gmail.com>
2024-07-20 18:51:13 -0300

    Comentando linha, temporariamente, pois estava gerando um aviso

--- ros2_workspace/src/map_share/map_share/map_subscriber.py
+++ ros2_workspace/src/map_share/map_share/map_subscriber.py
@@ -2,13 +2,16 @@ import rclpy
 from rclpy.node import Node
 
 #from std_msgs.msg import String
-from map_interfaces.msg import GetMapInfo
+from map_interfaces.msg import GetMapInfo, GetMapData
+
+def map_data_callback(msg):
+    print("Mapa agora é ", msg.data)
 
 def map_info_callback(msg):
     global last_timestamp
     if msg.timestamp != last_timestamp:
         last_timestamp = msg.timestamp
-        print("Timestamp mudou para ", last_timestamp) 
+        print("Timestamp mudou para", last_timestamp) 
 
 class Subscriber(Node):
     def __init__(self, topic_name, callback_function):
@@ -23,8 +26,9 @@ class Subscriber(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    sub = Subscriber('/map_info', map_info_callback)
-    rclpy.spin(sub)
+    sub1 = Subscriber('/map_info', map_info_callback)
+    #sub2 = Subscriber('/map_data', map_info_callback)
+    rclpy.spin(sub1)
     
     sub.destroy_node()
     rclpy.shutdown()
