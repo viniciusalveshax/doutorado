c7e75f79f8cc3910efb0ad1b30cf060ac060efb8
Vinícius Alves Hax <viniciushax@gmail.com>
2024-07-20 20:55:13 -0300

    Adicionando um tratamento para a situação da atualização do mapa

--- ros2_workspace/src/map_share/map_share/map_subscriber.py
+++ ros2_workspace/src/map_share/map_share/map_subscriber.py
@@ -2,7 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 #from std_msgs.msg import String
-from map_interfaces.msg import GetMapInfo, GetMapData
+from map_interfaces.msg import GetMapInfo #, GetMapData
+from map_interfaces.srv import GetMapData
 
 def map_data_callback(msg):
     print("Mapa agora é ", msg.data)
@@ -12,6 +13,32 @@ def map_info_callback(msg):
     if msg.timestamp != last_timestamp:
         last_timestamp = msg.timestamp
         print("Timestamp mudou para", last_timestamp) 
+        minimal_client = MinimalClientAsync()
+        future = minimal_client.send_request()
+        rclpy.spin_until_future_complete(minimal_client, future)
+        response = future.result()
+        print("Aqui")
+        print(type(response.data))
+        #minimal_client.get_logger().info(response.data)
+        print(response.data)
+        minimal_client.destroy_node()
+
+
+
+class MinimalClientAsync(Node):
+
+    def __init__(self):
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(GetMapData, 'get_map_data')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = GetMapData.Request()
+
+    def send_request(self):
+        #self.req.a = a
+        #self.req.b = b
+        return self.cli.call_async(self.req)
+
 
 class Subscriber(Node):
     def __init__(self, topic_name, callback_function):
@@ -29,6 +56,8 @@ def main(args=None):
     sub1 = Subscriber('/map_info', map_info_callback)
     #sub2 = Subscriber('/map_data', map_info_callback)
     rclpy.spin(sub1)
+    
+    
     
     sub.destroy_node()
     rclpy.shutdown()
